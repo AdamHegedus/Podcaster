06129c464692209609a65659b1ca8397edcfbcd9
Adam Hegedus <adam_hegedus@epam.com>
2014-10-02 13:32:40 +0200

    mashalling update

--- src/com/limitless/audio/core/PodcastDemo.java
+++ src/com/limitless/audio/core/PodcastDemo.java
@@ -1,9 +1,13 @@
 package com.limitless.audio.core;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
 import com.limitless.audio.core.xml.Rss;
@@ -16,20 +20,34 @@ public class PodcastDemo {
 			reading();
 		} catch (JAXBException e) {
 			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
 
-	private static void reading() throws JAXBException {
+	private static void reading() throws JAXBException, FileNotFoundException {
 		JAXBContext ctx = JAXBContext.newInstance(Rss.class);
 		Unmarshaller unmarshaller = ctx.createUnmarshaller();
+		Marshaller marshaller = ctx.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
 		Rss rss = (Rss) unmarshaller.unmarshal(new File("2.xml"));
 
-		System.out.println(rss.getVersion());
+		System.out.println(rss.getVersion() + " version");
+
+		System.out.println(rss.getChannel().getLanguage());
+		System.out.println("Desc: "
+				+ rss.getChannel().getItem().get(0).getDescription());
+
 		for (com.limitless.audio.core.xml.ItemType act : rss.getChannel()
 				.getItem()) {
 			System.out.println("Title: " + act.getTitle());
 
 		}
+
+		OutputStream outputXml = new FileOutputStream("1.xml");
+
+		marshaller.marshal(rss, outputXml);
+		marshaller.marshal(rss, System.out);
 	}
 }
